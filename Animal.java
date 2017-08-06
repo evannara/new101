fafb427d9cd9915964185190489438d2d35f5189
Vannara  Houth <evannara@gmail.com>
2017-08-06 15:19:46 -0400

    add modification

--- Animal.java
+++ Animal.java
@@ -1,23 +1,21 @@
 class Animal {
    public void move() {
-      System.out.println(Animals can move);
-      Yes it can move
+
    }
 }
 
 class Dog extends Animal {
    public void move() {
-      System.out.println(Dogs can fly and bite);
+      System.out.println("Woow it is gogole account");
+      System.out.println("I dont need this")
+      
    }
 }
 
 public class TestDog {
 
    public static void main(String args[]) {
-      Animal a = new Animal();   // Animal reference and object
-      Animal b = new Dog();   // Animal reference but Dog object
 
-      a.move();   // runs the method in Animal class
-      b.move();   // runs the method in Dog class
+
    }
 }
