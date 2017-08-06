ee3bac674186ec6756006fb7adc9080b351d66dd
Vannara  Houth <evannara@utexas.edu>
2017-08-06 15:28:12 -0400

    101

--- Animal.java
+++ Animal.java
@@ -1,23 +1,3 @@
-class Animal {
-   public void move() {
-      System.out.println(Animals can move);
-      Yes it can move
-      Wow that is incredible;
-   }
-}
-
-class Dog extends Animal {
-   public void move() {
-      System.out.println(This is my utexas account);
-   }
-}
-
-public class TestDog {
-
-   public static void main(String args[]) {}
-      Animal b = new Dog();   // Animal reference but Dog object
-
-      a.move();   // runs the method in Animal class
-      b.move();   // runs the method in Dog class
-   }
-}
+Add into utexas;
+utexas add this line;
+We dont need this line;
